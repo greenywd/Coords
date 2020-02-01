45349edc0172d29c7580bc320eca342e7b8e6cee
greenywd <greenydev@greenmail.net.au>
2020-02-01 16:29:53 +1030

    Initial Commit

---
+++ src/main/java/com/greeny/Coords/Coords.java
@@ -0,0 +1,54 @@
+package com.greeny.Coords;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Coords extends JavaPlugin {
+	public boolean onCommand(final CommandSender sender, final Command cmd, final String label, final String[] args) {
+		// If command is from a non-player (i.e. console)
+		if (!(sender instanceof Player)) {
+			if (cmd.getName().equalsIgnoreCase("coords") && args.length == 1) {
+				final Player player = Bukkit.getPlayer(args[0]);
+				if (Bukkit.getOnlinePlayers().contains(player)) {
+					final Location location = player.getLocation();
+					sender.sendMessage(player.getName() + "'s coords. X: " + location.getBlockX() + ", Y: "
+							+ location.getBlockY() + ", Z: " + location.getBlockZ());
+					return true;
+				} else {
+					sender.sendMessage("No player with username is on the server.");
+					return true;
+				}
+			} else {
+				sender.sendMessage("Running this command from console requires 1 argument (player name).");
+				return true;
+			}
+		} else {
+			if (cmd.getName().equalsIgnoreCase("coords")) {
+				// Player didn't specify a username, broadcast their own coordinates.
+				if (args.length == 0) {
+					final Player player = (Player) sender;
+					final Location location = player.getLocation();
+					Bukkit.broadcastMessage(player.getName() + "'s coords. X: " + location.getBlockX() + ", Y: "
+							+ location.getBlockY() + ", Z: " + location.getBlockZ());
+					return true;
+
+				} else if (args.length == 1) {
+					final Player player = Bukkit.getPlayer(args[0]);
+					if (Bukkit.getOnlinePlayers().contains(player)) {
+						final Location location = player.getLocation();
+						sender.sendMessage(player.getName() + "'s coords. X: " + location.getBlockX() + ", Y: " + location.getBlockY() + ", Z: " + location.getBlockZ());
+						return true;
+					} else {
+						sender.sendMessage("No player with username is on the server.");
+						return true;
+					}
+				}
+			}
+		}
+		return false; 
+	}
+}
